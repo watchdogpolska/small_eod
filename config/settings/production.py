da4474d0692a1de0e24c64fc52aa118009a89931
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-06-05 11:08:28 +0200

    Add Raven

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,3 +3,7 @@ from .base import *  # noqa
 SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'raven.contrib.django.raven_compat'
+]
