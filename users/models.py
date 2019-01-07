8c123cc52116394f52d5f0d8c147e400561bd1da
Radek <radoslaw.borowski.1986@gmial.com>
2019-01-07 21:09:23 +0100

    dynamic_ContentType_value

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
-from django.db import models
+
+
 
 # Create your models here.
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from small_eod.cases.models import Case, Letter
+
 
 
 @receiver(pre_save, sender=User)
@@ -12,18 +16,20 @@ def set_group(sender, **kwargs):
     user = kwargs["instance"]
     if user.is_staff == 0:
         user.is_staff = 1
-    else:
-        pass
+
 
 
 @receiver(post_save, sender=User)
 def set_permissions(sender, **kwargs):
     u, created = kwargs["instance"], kwargs["created"]
-    if created:
 
-        permission = Permission.objects.get(content_type=1, codename='view_Case')
-        permission1 = Permission.objects.get(content_type=1, codename='view_letter')
-        u.user_permissions.add(permission)
-        u.user_permissions.add(permission1)
-    else:
-        pass
+    ct = ContentType.objects.get(app_label='Cases', model="Case").id
+    ct1 = ContentType.objects.get(app_label='Cases', model="Letter").id
+
+    if created:
+        permission = Permission.objects.get(content_type=ct, codename='view_case')
+        permission1 = Permission.objects.get(content_type=ct1, codename='view_letter')
+        u.user_permissions.add(permission, permission1)
+
+
+
