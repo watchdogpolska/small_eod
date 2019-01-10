a38c6773e99eee8749b0399db5623c409932da57
Radek <radoslaw.borowski.1986@gmial.com>
2019-01-10 20:27:06 +0100

    code_optimalisation_domain_verification

--- users/models.py
+++ users/models.py
@@ -7,8 +7,7 @@ from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-from small_eod.cases.models import Case, Letter
-
+from django.db.models import Q
 
 
 @receiver(pre_save, sender=User)
@@ -18,18 +17,13 @@ def set_group(sender, **kwargs):
         user.is_staff = 1
 
 
-
 @receiver(post_save, sender=User)
 def set_permissions(sender, **kwargs):
     u, created = kwargs["instance"], kwargs["created"]
-
     ct = ContentType.objects.get(app_label='Cases', model="Case").id
     ct1 = ContentType.objects.get(app_label='Cases', model="Letter").id
 
-    if created:
-        permission = Permission.objects.get(content_type=ct, codename='view_case')
-        permission1 = Permission.objects.get(content_type=ct1, codename='view_letter')
-        u.user_permissions.add(permission, permission1)
-
-
-
+    if created and u.email.endswith('@siecobywatelska.pl'):
+        permission = Permission.objects.filter(Q(content_type=ct, codename='view_case')
+                                               | Q(content_type=ct1, codename='view_letter'))
+        u.user_permissions.set(permission)
