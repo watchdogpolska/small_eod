b9b08b81403e48fd3f628da82b0cf40a3d1ac963
Radek <radoslaw.borowski.1986@gmial.com>
2019-01-16 18:37:33 +0100

    15

--- users/models.py
+++ users/models.py
@@ -1,12 +1,8 @@
-
-
-
 # Create your models here.
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
 
@@ -20,10 +16,9 @@ def set_group(sender, **kwargs):
 @receiver(post_save, sender=User)
 def set_permissions(sender, **kwargs):
     u, created = kwargs["instance"], kwargs["created"]
-    ct = ContentType.objects.get(app_label='Cases', model="Case").id
-    ct1 = ContentType.objects.get(app_label='Cases', model="Letter").id
 
     if created and u.email.endswith('@siecobywatelska.pl'):
-        permission = Permission.objects.filter(Q(content_type=ct, codename='view_case')
-                                               | Q(content_type=ct1, codename='view_letter'))
+        permission = Permission.objects.filter(Q(content_type__app_label='Cases', content_type__model='Case',
+                                                 codename='view_case') | Q(content_type__app_label='Cases',
+                                               content_type__model='Letter', codename='view_letter'))
         u.user_permissions.set(permission)
