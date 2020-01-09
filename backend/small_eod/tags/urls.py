86693856dcc9cb78823fe44e30f49a9e860d0535
Mgrab <mgrabon@akamai.com>
2020-01-09 06:28:06 +0100

    first url in-app definition

--- backend/small_eod/tags/urls.py
+++ backend/small_eod/tags/urls.py
@@ -1,16 +1,9 @@
 from rest_framework import routers
 from django.urls import path, include
 
-from ..users.views import UserViewSet
-from ..institutions.views import AddressDataViewSet, InstitutionViewSet
-from ..cases.views import CaseViewSet
-from ..dictionaries.views import DictionaryViewSet
 from .views import TagViewSet
-router = routers.DefaultRouter()
-router.register('users', UserViewSet)
-router.register('address', AddressDataViewSet)
-router.register('institution', InstitutionViewSet)
-router.register('case', CaseViewSet)
+router = routers.SimpleRouter()
+
 router.register('', TagViewSet)
 
 app_name = 'tags'
