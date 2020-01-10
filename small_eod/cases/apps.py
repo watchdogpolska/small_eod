5e33b77a363b7d5e5b08c15344dd48485ce90ca5
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-10 10:18:07 +0100

    delete old small_eod

--- small_eod/cases/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class CasesConfig(AppConfig):
-    name = "small_eod.cases"
-    verbose_name = _("Cases")
--- small_eod/extracts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExtractsConfig(AppConfig):
-    name = "small_eod.extracts"
