b5d8273a8261db930d8a78c904d52fb9433cb362
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-10 10:15:36 +0100

    delete old config

--- config/settings/production.py
+++
@@ -1,8 +0,0 @@
-from .base import *  # noqa
-
-SECRET_KEY = env("SECRET_KEY")
-
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
-
-INSTALLED_APPS = INSTALLED_APPS + ["raven.contrib.django.raven_compat"]
-FILE_UPLOAD_PERMISSIONS = 0o644
