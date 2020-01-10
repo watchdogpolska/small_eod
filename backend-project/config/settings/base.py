bb87c8da7d984b83f91ec45511a8427eb69701f5
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-10 11:45:30 +0100

    update docker-compose

--- backend-project/config/settings/base.py
+++ backend-project/config/settings/base.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = "./static/"
+MEDIA_ROOT = "./media/"
 AUTH_USER_MODEL = "users.User"
 
 SWAGGER_SETTINGS = {"DEFAULT_INFO": "config.swagger.info"}
