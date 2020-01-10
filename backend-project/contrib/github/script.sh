f12459b5e6eff8b1c4a68beaacc9d6267af44fe4
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-10 19:00:37 +0100

    minio tests

--- backend-project/contrib/github/script.sh
+++ backend-project/contrib/github/script.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -eux
-make wait_mysql migrate
+docker-compose up -d
+make wait_mysql wait_minio migrate
 # verify that user able to create user
 docker-compose run web python manage.py createsuperuser --username root --email root@example.com --noinput
 # run test suite
 make test
 # verify integration of services
-docker-compose up -d web nginx
 sleep 5 # for http server start
 # verify that user able to view login form
 curl http://localhost:8000/ -s
