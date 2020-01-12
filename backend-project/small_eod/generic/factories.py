ecfbc41dc787bf0b74fb517d07b20792729963ba
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-12 22:10:39 +0100

    use abstract instead of mixin in factories

--- backend-project/small_eod/generic/factories.py
+++ backend-project/small_eod/generic/factories.py
@@ -3,6 +3,9 @@ import factory
 from ..users.factories import UserFactory
 
 
-class TimestampUserFactoryMixin:
+class TimestampUserFactoryMixin(factory.Factory):
     created_by = factory.SubFactory(UserFactory)
     modified_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        abstract = True
