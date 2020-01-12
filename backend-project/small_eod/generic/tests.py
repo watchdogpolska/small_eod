1fab5ae041d6ccafaba4e3030d7ff75592addac1
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-12 19:57:04 +0100

    add ExactLenghtsValidator

--- backend-project/small_eod/generic/tests.py
+++ backend-project/small_eod/generic/tests.py
@@ -1,10 +1,13 @@
 from typing import Type
 
+from django.core.validators import ValidationError
 from django.db.models import Model
+from django.test import TestCase
 from django.test import tag
 from django.urls import reverse
 from factory.django import DjangoModelFactory
 
+from .validators import ExactLenghtsValidator
 from ..users.factories import UserFactory
 
 
@@ -45,6 +48,7 @@ class ReadOnlyViewSetMixin:
 
     def validate_item(self, item):
         raise NotImplementedError('validate_item must be overridden')
+
 
 class GenericViewSetMixin(ReadOnlyViewSetMixin):
     def get_ommited_fields(self):
@@ -96,3 +100,50 @@ class FactoryCreateObjectsMixin:
     def test_factories_many(self):
         for x in range(1, self.FACTORY_COUNT):
             self._test_factory_object(msg='many', count=x)
+
+
+class ExactLenghtsValidatorTestCase(TestCase):
+
+    def test_validator_message(self):
+        """
+        Validator returns corrrect error message.
+        """
+        validator = ExactLenghtsValidator([10, 14, 566, 1])
+
+        with self.assertRaises(ValidationError) as err:
+            validator("12")
+
+        self.assertIn(
+            'Ensure this value has length of any [10, 14, 566, 1] (it has 2).',
+            err.exception,
+        )
+
+    def test_validator_values(self):
+        """
+        Given invalid values, validator raises `ValidationError`.
+        Given valid values, validator does not raise `ValidationError`.
+        """
+        validator = ExactLenghtsValidator([10, 14])
+
+        # valid values
+        chars_10 = "1111111111"; self.assertEqual(len(chars_10), 10)
+        chars_14 = "11111111111111"; self.assertEqual(len(chars_14), 14)
+
+        # invalid values
+        chars_9 = "111111111"; self.assertEqual(len(chars_9), 9)
+        chars_12 = "111111111111"; self.assertEqual(len(chars_12), 12)
+        chars_15 = "111111111111111"; self.assertEqual(len(chars_15), 15)
+
+        # valid test
+        validator(chars_10)
+        validator(chars_14)
+
+        # invalid test
+        with self.assertRaises(ValidationError):
+            validator(chars_9)
+
+        with self.assertRaises(ValidationError):
+            validator(chars_12)
+
+        with self.assertRaises(ValidationError):
+            validator(chars_15)
---
+++ backend-project/small_eod/generic/validators.py
@@ -0,0 +1,15 @@
+from django.core.validators import BaseValidator
+from django.utils.translation import ugettext_lazy as _
+
+
+class ExactLenghtsValidator(BaseValidator):
+    message = _('Ensure this value has length of any %(limit_value)s (it has %(show_value)d).')
+
+    def __init__(self, limit_value: list = None, **kwargs):
+        super().__init__(limit_value=limit_value, **kwargs)
+
+    def compare(self, a, b):
+        return all([a != x for x in b])
+
+    def clean(self, x):
+        return len(x)
