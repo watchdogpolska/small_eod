a009fdf3bb50baa1c86689bc85d23876786d14f1
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-12 06:47:09 +0100

    black reformat

--- backend-project/small_eod/institutions/factories.py
+++ backend-project/small_eod/institutions/factories.py
@@ -2,33 +2,34 @@ from factory.django import DjangoModelFactory
 import factory.fuzzy
 from .models import AddressData, ExternalIdentifier, Institution
 
-class PolishFaker(factory.Faker):
 
+class PolishFaker(factory.Faker):
     def __init__(self, *args, **kwargs):
-        kwargs['locale'] = 'PL'
+        kwargs["locale"] = "PL"
         super().__init__(*args, **kwargs)
 
 
 class AddressDataFactory(DjangoModelFactory):
-    city = PolishFaker('city')
-    voivodeship = PolishFaker('region')
-    flat_no = PolishFaker('building_number')
-    street = PolishFaker('street_name')
-    postal_code = PolishFaker('postcode')
-    house_no = PolishFaker('building_number')
-    email = PolishFaker('email')
+    city = PolishFaker("city")
+    voivodeship = PolishFaker("region")
+    flat_no = PolishFaker("building_number")
+    street = PolishFaker("street_name")
+    postal_code = PolishFaker("postcode")
+    house_no = PolishFaker("building_number")
+    email = PolishFaker("email")
     epuap = factory.Sequence(lambda n: "epuap-%s" % n)  # todo provide example epuap
 
     class Meta:
         model = AddressData
 
+
 class ExternalIdentifierFactory(DjangoModelFactory):
-# todo
+    # todo
     class Meta:
         model = ExternalIdentifier
 
 
 class InstitutionFactory(DjangoModelFactory):
-# todo
+    # todo
     class Meta:
         model = Institution
