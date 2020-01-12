eb97012d11dcec85b199573a66ff85620f489561
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-12 20:32:29 +0100

    Update tests.py

--- backend-project/small_eod/institutions/tests.py
+++ backend-project/small_eod/institutions/tests.py
@@ -10,14 +10,6 @@ class AddressDataFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
     FACTORY = AddressDataFactory
     MODEL = AddressData
 
-    def test_print_to_console(self):
-        """
-        Show in the console how the data looks like.
-        Just making sure that frontend has the right data
-        to work with.
-        """
-        print(f"\n{model_to_dict(self.FACTORY())}")
-
 
 class ExternalIdentifierFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
     FACTORY = ExternalIdentifierFactory
