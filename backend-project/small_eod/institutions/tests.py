45c0fc34d52cca153b41b2333f5ba3d04a5b0b30
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-12 06:44:11 +0100

    draft institutions factory

--- backend-project/small_eod/institutions/tests.py
+++ backend-project/small_eod/institutions/tests.py
@@ -1,3 +1,29 @@
-# from django.test import TestCase
+from django.forms import model_to_dict
+from django.test import TestCase
 
-# Create your tests here.
+from .factories import AddressDataFactory, ExternalIdentifierFactory, InstitutionFactory
+from .models import AddressData, ExternalIdentifier, Institution
+from ..generic.tests import FactoryCreateObjectsMixin
+
+
+class AddressDataFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = AddressDataFactory
+    MODEL = AddressData
+
+    def test_print_to_console(self):
+        """
+        Show in the console how the data looks like.
+        Just making sure that frontend has the right data
+        to work with.
+        """
+        print(f"\n{model_to_dict(self.FACTORY())}")
+
+
+class ExternalIdentifierFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = ExternalIdentifierFactory
+    MODEL = ExternalIdentifier
+
+
+class InstitutionFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = InstitutionFactory
+    MODEL = Institution
