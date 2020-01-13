f2f8ea688d8f86191d7b37b9bff54c5ab3c9f461
Mgrab <mgrabon@akamai.com>
2020-01-13 04:11:16 +0100

    pep8 changes

--- backend-project/small_eod/cases/urls.py
+++ backend-project/small_eod/cases/urls.py
@@ -8,8 +8,16 @@ router = SimpleRouter()
 router.register('cases', CaseViewSet, basename='cases')
 
 user_router = NestedSimpleRouter(router, 'cases', lookup='case')
-user_router.register('responsibleUser', ResponsibleUserViewSet, basename='cases-responsible_user')
-user_router.register(r'notifiedUser', NotifiedUserViewSet, basename='cases-notified_user')
+user_router.register(
+    'responsibleUser',
+    ResponsibleUserViewSet,
+    basename='cases-responsible_user'
+)
+user_router.register(
+    r'notifiedUser',
+    NotifiedUserViewSet,
+    basename='cases-notified_user'
+)
 
 urlpatterns = [
     path("", include(router.urls)),
