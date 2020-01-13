715bb36f462fc01ed83ff7304c2d998c4c68aa1e
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-13 03:45:46 +0100

    use FuzzyDateTimeFromNow

--- backend-project/small_eod/events/factories.py
+++ backend-project/small_eod/events/factories.py
@@ -1,21 +1,16 @@
-import datetime
-
 import factory.fuzzy
-from django.utils import timezone
 
 from .models import Event
 from ..cases.factories import CaseFactory
-from ..generic.factories import AbstractTimestampUserFactory
+from ..generic.factories import AbstractTimestampUserFactory, FuzzyDateTimeFromNow
 
 
 class EventFactory(AbstractTimestampUserFactory, factory.DjangoModelFactory):
 
     name = factory.Sequence(lambda n: "event-%04d" % n)
-    case = factory.SubFactory(CaseFactory)
     comment = factory.Sequence(lambda n: "comment-event-%04d" % n)
-    data = factory.fuzzy.FuzzyDateTime(
-        start_dt=timezone.now(), end_dt=timezone.now() + datetime.timedelta(days=10),
-    )
+    case = factory.SubFactory(CaseFactory)
+    data = FuzzyDateTimeFromNow(max_days=10)
 
     class Meta:
         model = Event
