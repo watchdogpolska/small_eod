ada4043a22e483c03590106ffe293e59c0428fc8
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-13 02:45:34 +0100

    factories for dictionaries

--- backend-project/small_eod/dictionaries/factories.py
+++ backend-project/small_eod/dictionaries/factories.py
@@ -1,14 +1,16 @@
-import factory
 import factory.fuzzy
 
 from .models import Feature, Dictionary
+from ..generic.factories import AbstractTimestampUserFactory
 
 
-class DictionaryFactory(factory.django.DjangoModelFactory):
+class DictionaryFactory(
+    AbstractTimestampUserFactory, factory.django.DjangoModelFactory
+):
     name = factory.Sequence(lambda n: "dictionary-%04d" % n)
-    active = True
-    min_items = factory.fuzzy.FuzzyInteger(0, 3)
-    max_items = factory.fuzzy.FuzzyInteger(4, 5)
+    active = factory.fuzzy.FuzzyChoice((True, False))
+    min_items = factory.fuzzy.FuzzyInteger(1, 3)
+    max_items = factory.fuzzy.FuzzyInteger(3, 20)
 
     class Meta:
         model = Dictionary
@@ -16,6 +18,7 @@ class DictionaryFactory(factory.django.DjangoModelFactory):
 
 class FeatureFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: "feature-%04d" % n)
+    dictionary = factory.SubFactory(DictionaryFactory)
 
     class Meta:
         model = Feature
--- backend-project/small_eod/dictionaries/tests.py
+++ backend-project/small_eod/dictionaries/tests.py
@@ -1,7 +1,28 @@
 from django.test import TestCase
-from .models import Feature
+
+from .factories import DictionaryFactory, FeatureFactory
+from .models import Dictionary, Feature
 from .serializers import DictionarySerializer
-# Create your tests here.
+from ..generic.tests import FactoryCreateObjectsMixin
+
+
+class DictionaryFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    MODEL = Dictionary
+    FACTORY = DictionaryFactory
+
+
+class FeatureFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    MODEL = Feature
+    FACTORY = FeatureFactory
+
+    def test_foreign_keys(self):
+        """
+        Foreign keys are created.
+        """
+        obj = self.create_factory()
+        self.assertIsNotNone(obj.dictionary)
+
+
 class DictionarySerializerTestCase(TestCase):
     def test_save_nested_values(self):
         serializer = DictionarySerializer(data={
