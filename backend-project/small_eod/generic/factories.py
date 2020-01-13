f81a8a9862541cd1f1377c2bc26991270c41b019
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-13 02:01:44 +0100

    create ManyToManyPostGeneration

--- backend-project/small_eod/generic/factories.py
+++ backend-project/small_eod/generic/factories.py
@@ -3,9 +3,30 @@ import factory
 from ..users.factories import UserFactory
 
 
+def _m2m_post_add(attr, obj, create, extracted, **kwargs):
+    if not create:
+        return
+    if extracted:
+        for each in extracted:
+            getattr(obj, attr).add(each)
+
+
 class AbstractTimestampUserFactory(factory.Factory):
     created_by = factory.SubFactory(UserFactory)
     modified_by = factory.SubFactory(UserFactory)
 
     class Meta:
         abstract = True
+
+
+class ManyToManyPostGeneration(factory.PostGeneration):
+    def __init__(self, m2m_field_name):
+        super().__init__(function=None)
+        self.m2m_field_name = m2m_field_name
+        self.function = lambda obj, create, extracted, **kwargs: _m2m_post_add(
+            attr=self.m2m_field_name,
+            obj=obj,
+            create=create,
+            extracted=extracted,
+            **kwargs,
+        )
