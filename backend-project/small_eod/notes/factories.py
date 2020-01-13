b3e4a724c5c6e2d46416181503f17fc28968bb67
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-13 04:09:39 +0100

    AbstractTimestampUserFactory for NoteFactory

--- backend-project/small_eod/notes/factories.py
+++ backend-project/small_eod/notes/factories.py
@@ -1,10 +1,10 @@
 import factory.fuzzy
-
+from ..generic.factories import AbstractTimestampUserFactory
 from .models import Note
 from ..cases.factories import CaseFactory
 
 
-class NoteFactory(factory.django.DjangoModelFactory):
+class NoteFactory(AbstractTimestampUserFactory, factory.DjangoModelFactory):
     case = factory.SubFactory(CaseFactory)
     comment = factory.Sequence(lambda n: "comment-%04d" % n)
 
