d3bb539085da30b61847107a449de49fa9d522f1
kotEustachy <kylo.997@wp.pl>
2020-01-14 15:21:37 +0100

    change hardcoded media root path to autogenerated from base dir path and use double quotes

--- backend-project/config/settings/base.py
+++ backend-project/config/settings/base.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "./static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 AUTH_USER_MODEL = "users.User"
 
 SWAGGER_SETTINGS = {"DEFAULT_INFO": "config.swagger.info"}
