b70588364444fe4484f0edf8d62208ee1897a0fd
kotEustachy <kylo.997@wp.pl>
2020-01-14 16:57:55 +0100

    change hardcoded static root path to autogenerated from base dir path

--- backend-project/config/settings/base.py
+++ backend-project/config/settings/base.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "./static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 AUTH_USER_MODEL = "users.User"
 
