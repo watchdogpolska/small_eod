24ace6f2e002696a0921b96c0f92893097356aef
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-19 04:03:37 +0100

    create users.factories_test

---
+++ backend-project/small_eod/users/factories_test.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from .factories import UserFactory
+from .models import User
+from ..generic.tests import FactoryCreateObjectsMixin
+
+
+class UserFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    MODEL = User
+    FACTORY = UserFactory
