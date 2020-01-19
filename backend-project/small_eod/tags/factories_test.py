f95814c6a0c5dc4ea3c16243630f1c6bc80f34af
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-19 04:04:46 +0100

    create tags.factories_test

---
+++ backend-project/small_eod/tags/factories_test.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from .factories import TagFactory, TagNamespaceFactory
+from .models import Tag, TagNamespace
+from ..generic.tests import FactoryCreateObjectsMixin
+
+
+class TagFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    MODEL = Tag
+    FACTORY = TagFactory
+
+
+class TagNamespaceFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    MODEL = TagNamespace
+    FACTORY = TagNamespaceFactory
--- backend-project/small_eod/tags/tests.py
+++ backend-project/small_eod/tags/tests.py
@@ -1,18 +1,6 @@
 from django.test import TestCase
 
 from .factories import TagFactory, TagNamespaceFactory
-from .models import Tag, TagNamespace
-from ..generic.tests import FactoryCreateObjectsMixin
-
-
-class TagFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
-    MODEL = Tag
-    FACTORY = TagFactory
-
-
-class TagNamespaceFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
-    MODEL = TagNamespace
-    FACTORY = TagNamespaceFactory
 
 
 class TagsTestCase(TestCase):
