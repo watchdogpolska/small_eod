8fd24b645c98495f4866cf872abee1efa7a211d3
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-19 14:47:12 +0100

    move institutions.tests

---
+++ backend-project/small_eod/institutions/tests/factories.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from ..factories import AddressDataFactory, ExternalIdentifierFactory, InstitutionFactory
+from ..models import AddressData, ExternalIdentifier, Institution
+from ...generic.tests import FactoryCreateObjectsMixin
+
+
+class AddressDataFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = AddressDataFactory
+    MODEL = AddressData
+
+
+class ExternalIdentifierFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = ExternalIdentifierFactory
+    MODEL = ExternalIdentifier
+
+
+class InstitutionFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = InstitutionFactory
+    MODEL = Institution
