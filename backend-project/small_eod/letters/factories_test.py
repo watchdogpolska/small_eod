6300390780bfeaa8684be7c77ce8cebc9c0b2a18
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-19 04:05:02 +0100

    create letters.factories_test

---
+++ backend-project/small_eod/letters/factories_test.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from .factories import DescriptionFactory, LetterFactory
+from .models import Description, Letter
+from ..generic.tests import FactoryCreateObjectsMixin
+
+
+class LetterFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = LetterFactory
+    MODEL = Letter
+
+
+class DescriptionFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = DescriptionFactory
+    MODEL = Description
