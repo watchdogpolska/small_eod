1c44cc4a763f55a53634112783f57a3790afedd7
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-19 04:05:54 +0100

    create events.factories_test

---
+++ backend-project/small_eod/events/factories_test.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from .factories import EventFactory
+from .models import Event
+from ..generic.tests import FactoryCreateObjectsMixin
+
+
+class EventFactoryTestCase(FactoryCreateObjectsMixin, TestCase):
+    FACTORY = EventFactory
+    MODEL = Event
