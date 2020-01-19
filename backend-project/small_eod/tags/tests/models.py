7a1b4d006d1a86d62cc9b1231255fa4349c76cc0
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-19 14:50:22 +0100

    move tags.tests

---
+++ backend-project/small_eod/tags/tests/models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from .factories import TagFactory, TagNamespaceFactory
+
+
+class TagTestCase(TestCase):
+    def test_prefix(self):
+        """
+        `Tag` can be matched by `TagNamespace.prefix`.
+        """
+        tag = TagFactory(name="test")
+        tag_namespace = TagNamespaceFactory(prefix="test")
+        self.assertTrue(tag.name.startswith(tag_namespace.prefix))
+
