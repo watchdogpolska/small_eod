19b4c508c523713eaf804eb5688a230a15e9c862
Kasia <efefre@icloud.com>
2020-01-22 21:13:21 +0100

    PEP8 refactoring

--- backend-project/small_eod/cases/models.py
+++ backend-project/small_eod/cases/models.py
@@ -18,14 +18,19 @@ class CaseQuerySet(models.QuerySet):
 class Case(TimestampUserLogModel):
     objects = CaseQuerySet.as_manager()
 
-    name = models.CharField(max_length=256, verbose_name=_("Name"), help_text=_("Case's name. Name cannot be longer than 256 characters."))
-    comment = models.CharField(max_length=256, verbose_name=_("Comment"), help_text=_("Comment cannot be longer than 256 characters."))
+    name = models.CharField(max_length=256, verbose_name=_("Name"),
+                            help_text=_("Case's name. Name cannot be longer than 256 characters.")
+                            )
+    comment = models.CharField(max_length=256, verbose_name=_("Comment"),
+                               help_text=_("Comment cannot be longer than 256 characters.")
+                               )
 
-    tag = models.ManyToManyField(to=Tag, blank=True, verbose_name=_("Tag"), help_text="Choose tag")
+    tag = models.ManyToManyField(to=Tag, blank=True, verbose_name=_("Tag"), help_text="Choose tag.")
     feature = models.ManyToManyField(to=Feature, blank=True, verbose_name=_("Feature"))
 
     audited_institution = models.ManyToManyField(
-        to=Institution, blank=True, verbose_name="Audited institution", help_text=_("Case audits this Institution"),
+        to=Institution, blank=True,
+        verbose_name="Audited institution", help_text=_("Case audits this Institution."),
     )
     notified_user = models.ManyToManyField(
         to=settings.AUTH_USER_MODEL, related_name="case_notified_user", blank=True,
