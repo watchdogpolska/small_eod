8ceea37e7daf68774c65afad466d8d53854db2ba
Kasia <efefre@icloud.com>
2020-01-22 21:37:00 +0100

    Refactor with make fmt

--- backend-project/small_eod/cases/models.py
+++ backend-project/small_eod/cases/models.py
@@ -18,29 +18,46 @@ class CaseQuerySet(models.QuerySet):
 class Case(TimestampUserLogModel):
     objects = CaseQuerySet.as_manager()
 
-    name = models.CharField(max_length=256, verbose_name=_("Name"),
-                            help_text=_("Case's name. Name cannot be longer than 256 characters.")
-                            )
-    comment = models.CharField(max_length=256, verbose_name=_("Comment"),
-                               help_text=_("Comment cannot be longer than 256 characters.")
-                               )
+    name = models.CharField(
+        max_length=256,
+        verbose_name=_("Name"),
+        help_text=_("Case's name. Name cannot be longer than 256 characters."),
+    )
+    comment = models.CharField(
+        max_length=256,
+        verbose_name=_("Comment"),
+        help_text=_("Comment cannot be longer than 256 characters."),
+    )
 
-    tag = models.ManyToManyField(to=Tag, blank=True, verbose_name=_("Tag"), help_text=_("Choose tag."))
-    feature = models.ManyToManyField(to=Feature, blank=True,
-                                     verbose_name=_("Feature"), help_text= _("Features for this case.")
-                                     )
+    tag = models.ManyToManyField(
+        to=Tag, blank=True, verbose_name=_("Tag"), help_text=_("Choose tag.")
+    )
+    feature = models.ManyToManyField(
+        to=Feature,
+        blank=True,
+        verbose_name=_("Feature"),
+        help_text=_("Features for this case."),
+    )
 
     audited_institution = models.ManyToManyField(
-        to=Institution, blank=True,
-        verbose_name="Audited institution", help_text=_("Case audits this Institution."),
+        to=Institution,
+        blank=True,
+        verbose_name="Audited institution",
+        help_text=_("Case audits this Institution."),
     )
     notified_user = models.ManyToManyField(
-        to=settings.AUTH_USER_MODEL, related_name="case_notified_user", blank=True,
-        verbose_name=_("Notified user"), help_text=_("User who will receive notification.")
+        to=settings.AUTH_USER_MODEL,
+        related_name="case_notified_user",
+        blank=True,
+        verbose_name=_("Notified user"),
+        help_text=_("User who will receive notification."),
     )
     responsible_user = models.ManyToManyField(
-        to=settings.AUTH_USER_MODEL, related_name="case_responsible_user", blank=True,
-        verbose_name=_("Responsible user"), help_text=_("User who is responsible for this case.")
+        to=settings.AUTH_USER_MODEL,
+        related_name="case_responsible_user",
+        blank=True,
+        verbose_name=_("Responsible user"),
+        help_text=_("User who is responsible for this case."),
     )
 
     def __str__(self):
