22626f58344a018b3b43a411792f2240ad86238a
nnow991 <nnowak91@gmail.com>
2020-01-22 16:22:58 +0100

    ant_design_added

---
+++ frontend-project/design_pro/config/defaultSettings.js
@@ -0,0 +1,17 @@
+export default {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: 'daybreak',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+};
