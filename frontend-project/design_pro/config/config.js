410ec6fc007736d9910a35d05e1eccb53afb6096
nnow991 <nnowak91@gmail.com>
2020-01-24 18:38:59 +0100

    fixed_some_errors

--- frontend-project/design_pro/config/config.js
+++ frontend-project/design_pro/config/config.js
@@ -368,32 +368,7 @@ export default {
                   component: './form/advanced-form',
                 },
               ],
-            },/*
-            {
-              name: 'editor',
-              icon: 'highlight',
-              path: '/editor',
-              routes: [
-                {
-                  name: 'flow',
-                  icon: 'smile',
-                  path: '/editor/flow',
-                  component: './editor/flow',
-                },
-                {
-                  name: 'mind',
-                  icon: 'smile',
-                  path: '/editor/mind',
-                  component: './editor/mind',
-                },
-                {
-                  name: 'koni',
-                  icon: 'smile',
-                  path: '/editor/koni',
-                  component: './editor/koni',
-                },
-              ],
-            },*/
+            },
             {
               path: '/',
               redirect: '/dashboard/analysis',
