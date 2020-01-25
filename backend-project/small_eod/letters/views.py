4e690b9915ccf2da6b823e177b48007ffee797fb
matiuszka <StqY2boeiZ>
2020-01-25 16:42:06 +0100

    Again lint fix.

--- backend-project/small_eod/letters/views.py
+++ backend-project/small_eod/letters/views.py
@@ -3,7 +3,6 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import mixins
 from rest_framework_nested.viewsets import NestedViewSetMixin
 
 from .models import Letter, Description
