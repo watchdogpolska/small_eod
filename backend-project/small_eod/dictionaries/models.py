aea8296a8f30b4bc0fa560a632b63ff8eaf23151
Kasia <efefre@icloud.com>
2020-01-31 21:46:22 +0100

    Add verbose_name and help_text for field: max_items

--- backend-project/small_eod/dictionaries/models.py
+++ backend-project/small_eod/dictionaries/models.py
@@ -7,7 +7,7 @@ from ..generic.models import TimestampUserLogModel
 class Dictionary(TimestampUserLogModel):
     name = models.CharField(max_length=100, verbose_name=_("Name"), help_text=_("Name of dictionary."))
     min_items = models.IntegerField(default=1, verbose_name=_("Min. items"), help_text=_("Minimum number of items."))
-    max_items = models.IntegerField(default=1)
+    max_items = models.IntegerField(default=1, verbose_name=_("Max. items"), help_text=_("Maximum number of items."))
     active = models.BooleanField(default=False)
 
     class Meta:
