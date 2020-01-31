efce63337f11a662adac13226f26c2d56e533973
Kasia <efefre@icloud.com>
2020-01-31 21:25:18 +0100

    Add verbose_name and help_text for field: expired_on

--- backend-project/small_eod/collections/models.py
+++ backend-project/small_eod/collections/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from ..generic.models import TimestampUserLogModel
 
 
 class Collection(TimestampUserLogModel):
-    expired_on = models.DateTimeField()
+    expired_on = models.DateTimeField(verbose_name=_("An expiration date"), help_text=_("An expiration date of collection."))
     query = models.CharField(max_length=256)
     comment = models.CharField(max_length=256)
     public = models.BooleanField(default=False)
