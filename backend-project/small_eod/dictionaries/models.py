fad26faf780648e485211104c45acba687a7afba
Kasia <efefre@icloud.com>
2020-01-31 21:40:35 +0100

    Add verbose_name and help_text for field: name

--- backend-project/small_eod/dictionaries/models.py
+++ backend-project/small_eod/dictionaries/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from ..generic.models import TimestampUserLogModel
 
 
 class Dictionary(TimestampUserLogModel):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name=_("Name"), help_text=_("Name of dictionary."))
     min_items = models.IntegerField(default=1)
     max_items = models.IntegerField(default=1)
     active = models.BooleanField(default=False)
