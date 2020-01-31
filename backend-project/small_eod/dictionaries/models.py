7180bd2b1f36659b2772f274e9f86ce7cfbaedd6
Kasia <efefre@icloud.com>
2020-01-31 21:47:47 +0100

    Add verbose_name and help_text for field: active

--- backend-project/small_eod/dictionaries/models.py
+++ backend-project/small_eod/dictionaries/models.py
@@ -8,7 +8,7 @@ class Dictionary(TimestampUserLogModel):
     name = models.CharField(max_length=100, verbose_name=_("Name"), help_text=_("Name of dictionary."))
     min_items = models.IntegerField(default=1, verbose_name=_("Min. items"), help_text=_("Minimum number of items."))
     max_items = models.IntegerField(default=1, verbose_name=_("Max. items"), help_text=_("Maximum number of items."))
-    active = models.BooleanField(default=False)
+    active = models.BooleanField(default=False, verbose_name=_("Active"), help_text=_("Make active."))
 
     class Meta:
         verbose_name_plural = "Dictionaries"
