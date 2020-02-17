eee849eec571f689c00545b81f8779df75862cda
Kasia <efefre@icloud.com>
2020-02-17 20:19:52 +0100

    Refactor code

--- backend-project/small_eod/events/models.py
+++ backend-project/small_eod/events/models.py
@@ -6,6 +6,15 @@ from ..cases.models import Case
 
 class Event(TimestampUserLogModel):
     data = models.DateTimeField(verbose_name=_("Date"), help_text=_("Date of event."))
-    name = models.CharField(max_length=256, verbose_name=_("Name"), help_text=_("Name of event."))
-    comment = models.CharField(max_length=256, verbose_name=_("Comment"), help_text=_("Comment text."))
-    case = models.ForeignKey(to=Case, on_delete=models.DO_NOTHING, verbose_name=_("Case"), help_text=_("Case for this event."))
+    name = models.CharField(
+        max_length=256, verbose_name=_("Name"), help_text=_("Name of event.")
+    )
+    comment = models.CharField(
+        max_length=256, verbose_name=_("Comment"), help_text=_("Comment text.")
+    )
+    case = models.ForeignKey(
+        to=Case,
+        on_delete=models.DO_NOTHING,
+        verbose_name=_("Case"),
+        help_text=_("Case for this event."),
+    )
