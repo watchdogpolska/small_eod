2c3efa6c37e0b5e1d109dbb89dfebfdf499892d2
Piotr Fryc <piotr.fryc@gmail.com>
2020-02-18 22:23:57 +0100

    housekeeping

--- backend-project/small_eod/collections/tests/test_views.py
+++ backend-project/small_eod/collections/tests/test_views.py
@@ -2,19 +2,39 @@ from django.test import TestCase
 
 from ..factories import CollectionFactory
 from ..serializers import CollectionSerializer
-from ...generic.tests.test_views import ReadOnlyViewSetMixin, GenericViewSetMixin
+from ...generic.tests.test_views import (
+    AuthorshipViewSetMixin,
+    ReadOnlyViewSetMixin,
+    GenericViewSetMixin
+)
 from ...notes.factories import NoteFactory
+from ...cases.factories import CaseFactory
+from ...cases.serializers import CaseSerializer
 
 
 class CollectionViewSetTestCase(GenericViewSetMixin, TestCase):
-    # TODO: Test nie przechodzi z powodu braku wypełnionego pola querry podczas
-    # tworzenia - należy uzupełnić factory
     basename = "collection"
     serializer_class = CollectionSerializer
     factory_class = CollectionFactory
 
     def validate_item(self, item):
         self.assertEqual(item["comment"], self.obj.comment)
+
+
+class CaseViewSetTestCase(AuthorshipViewSetMixin, GenericViewSetMixin, TestCase):
+    basename = "collection-cases"
+    serializer_class = CaseSerializer
+    factory_class = CaseFactory
+
+    def setUp(self):
+        super().setUp()
+        self.collection = CollectionFactory(query=str(self.obj.id))
+
+    def get_extra_kwargs(self):
+        return dict(collection_pk=self.collection.pk)
+
+    def validate_item(self, item):
+        self.assertEqual(item["name"], self.obj.name)
 
 
 class NoteViewSetTestCase(ReadOnlyViewSetMixin, TestCase):
