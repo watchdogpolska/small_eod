94a4c85ea7428934f7927c8461549039f2ce2eb8
Piotr Fryc <piotr.fryc@gmail.com>
2020-02-26 06:31:04 +0100

    Institution tests

--- backend-project/small_eod/institutions/tests/test_views.py
+++ backend-project/small_eod/institutions/tests/test_views.py
@@ -1,3 +1,23 @@
 # TODO: Dodać test class InstitutionViewSetTestCase(AuthorshipViewSetMixin, TestCase).
 # Atualnie taki test nie przechodzi z powodu problemów z factory (nieprawidłowe pk dla
 #  AdministrativeUnit
+
+from django.test import TestCase
+
+from ..factories import InstitutionFactory
+from ..serializers import InstitutionSerializer
+from ...generic.tests.test_views import (
+    GenericViewSetMixin,
+    # ReadOnlyViewSetMixin,
+    AuthorshipViewSetMixin,
+)
+# from ...users.factories import UserFactory
+# from ...users.serializers import UserSerializer
+
+class InstitutionViewSetTestCase(AuthorshipViewSetMixin, GenericViewSetMixin, TestCase):
+    basename = "nstitution"
+    serializer_class = InstitutionSerializer
+    factory_class = InstitutionFactory
+
+    def validate_item(self, item):
+        self.assertEqual(item["name"], self.obj.name)
