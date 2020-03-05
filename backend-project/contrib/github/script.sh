feb29d789e87ae8e4592fce9ac6cbe8635a52a1a
Adam Dobrawy <ad-m@users.noreply.github.com>
2020-03-05 04:38:32 +0100

    Frontend development environment hidden in docker as nginx upstream (#246)

--- backend-project/contrib/github/script.sh
+++ backend-project/contrib/github/script.sh
@@ -13,5 +13,5 @@ curl http://localhost:8000/ -s
 # verify that static assets available for nginx
 [ $(curl  -s -o /dev/null -w '%{http_code}' localhost:8000/static/admin/css/base.css) -eq '200' ]
 # keep OpenAPI specs for build-artifact
-docker-compose run web python -W ignore manage.py generate_swagger -f yaml > openapi.yaml
-docker-compose run web python -W ignore manage.py generate_swagger -f json > openapi.json
+docker-compose run backend python -W ignore manage.py generate_swagger -f yaml > openapi.yaml
+docker-compose run backend python -W ignore manage.py generate_swagger -f json > openapi.json
