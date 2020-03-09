517d3d03d8fd30c224edf976127c5c3809306711
Tomasz Magulski <tomasz@magul.ski>
2020-03-09 11:35:10 +0100

    Add case form styling

--- frontend-project/src/pages/cases/new/index.jsx
+++ frontend-project/src/pages/cases/new/index.jsx
@@ -1,9 +1,18 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Button, Card, Form, Input, Row, Select } from 'antd';
+import { Button, Col, Card, Form, Input, Row, Select } from 'antd';
 import React from 'react';
 import { formatMessage, FormattedMessage } from 'umi-plugin-react/locale';
 
 const { TextArea } = Input;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 const CasesNewForm = () => {
   const [form] = Form.useForm();
@@ -13,113 +22,133 @@ const CasesNewForm = () => {
   };
 
   return (
-    <Form form={form}>
+    <Form {...layout} form={form}>
       <PageHeaderWrapper content={formatMessage({ id: 'cases-new.page-header-content' })}>
         <Card bordered={false}>
           <Row>
-            <Form.Item
-              label={formatMessage({ id: 'cases-new.form.name.label' })}
-              name="name"
-              rules={[
-                {
-                  required: true,
-                  message: formatMessage({ id: 'cases-new.form.name.required-error' }),
-                },
-              ]}
-            >
-              <Input placeholder={formatMessage({ id: 'cases-new.form.name.placeholder' })} />
-            </Form.Item>
+            <Col span={16}>
+              <Form.Item
+                label={formatMessage({ id: 'cases-new.form.name.label' })}
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'cases-new.form.name.required-error' }),
+                  },
+                ]}
+              >
+                <Input placeholder={formatMessage({ id: 'cases-new.form.name.placeholder' })} />
+              </Form.Item>
+            </Col>
           </Row>
           <Row>
-            <Form.Item
-              label={formatMessage({ id: 'cases-new.form.comment.label' })}
-              name="comment"
-              rules={[
-                {
-                  required: true,
-                  message: formatMessage({ id: 'cases-new.form.comment.required-error' }),
-                },
-              ]}
-            >
-              <TextArea
-                rows={4}
-                placeholder={formatMessage({ id: 'cases-new.form.comment.placeholder' })}
-              />
-            </Form.Item>
+            <Col span={16}>
+              <Form.Item
+                label={formatMessage({ id: 'cases-new.form.comment.label' })}
+                name="comment"
+                rules={[
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'cases-new.form.comment.required-error' }),
+                  },
+                ]}
+              >
+                <TextArea
+                  rows={4}
+                  placeholder={formatMessage({ id: 'cases-new.form.comment.placeholder' })}
+                />
+              </Form.Item>
+            </Col>
           </Row>
           <Row>
-            <Form.Item
-              label={formatMessage({ id: 'cases-new.form.tags.label' })}
-              name="tags"
-              rules={[
-                {
-                  required: true,
-                  message: formatMessage({ id: 'cases-new.form.tags.required-error' }),
-                },
-              ]}
-            >
-              <Select
-                mode="tags"
-                placeholder={formatMessage({ id: 'cases-new.form.tags.placeholder' })}
-              />
-            </Form.Item>
+            <Col span={16}>
+              <Form.Item
+                label={formatMessage({ id: 'cases-new.form.tags.label' })}
+                name="tags"
+                rules={[
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'cases-new.form.tags.required-error' }),
+                  },
+                ]}
+              >
+                <Select
+                  mode="tags"
+                  placeholder={formatMessage({ id: 'cases-new.form.tags.placeholder' })}
+                />
+              </Form.Item>
+            </Col>
           </Row>
           <Row>
-            <Form.Item
-              label={formatMessage({ id: 'cases-new.form.features.label' })}
-              name="features"
-              rules={[
-                {
-                  required: true,
-                  message: formatMessage({ id: 'cases-new.form.features.required-error' }),
-                },
-              ]}
-            >
-              <Select
-                mode="multiple"
-                placeholder={formatMessage({ id: 'cases-new.form.features.placeholder' })}
-              />
-            </Form.Item>
+            <Col span={16}>
+              <Form.Item
+                label={formatMessage({ id: 'cases-new.form.features.label' })}
+                name="features"
+                rules={[
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'cases-new.form.features.required-error' }),
+                  },
+                ]}
+              >
+                <Select
+                  mode="multiple"
+                  placeholder={formatMessage({ id: 'cases-new.form.features.placeholder' })}
+                />
+              </Form.Item>
+            </Col>
           </Row>
           <Row>
-            <Form.Item
-              label={formatMessage({ id: 'cases-new.form.audited-institution.label' })}
-              name="audited-institution"
-            >
-              <Select
-                mode="multiple"
-                placeholder={formatMessage({
-                  id: 'cases-new.form.audited-institution.placeholder',
-                })}
-              />
-            </Form.Item>
+            <Col span={16}>
+              <Form.Item
+                label={formatMessage({ id: 'cases-new.form.audited-institution.label' })}
+                name="audited-institution"
+              >
+                <Select
+                  mode="multiple"
+                  placeholder={formatMessage({
+                    id: 'cases-new.form.audited-institution.placeholder',
+                  })}
+                />
+              </Form.Item>
+            </Col>
           </Row>
           <Row>
-            <Form.Item
-              label={formatMessage({ id: 'cases-new.form.notified-users.label' })}
-              name="notified-users"
-            >
-              <Select
-                mode="multiple"
-                placeholder={formatMessage({ id: 'cases-new.form.notified-users.placeholder' })}
-              />
-            </Form.Item>
+            <Col span={16}>
+              <Form.Item
+                label={formatMessage({ id: 'cases-new.form.notified-users.label' })}
+                name="notified-users"
+              >
+                <Select
+                  mode="multiple"
+                  placeholder={formatMessage({ id: 'cases-new.form.notified-users.placeholder' })}
+                />
+              </Form.Item>
+            </Col>
           </Row>
           <Row>
-            <Form.Item
-              label={formatMessage({ id: 'cases-new.form.responsible-users.label' })}
-              name="notified-users"
-            >
-              <Select
-                mode="multiple"
-                placeholder={formatMessage({ id: 'cases-new.form.responsible-users.placeholder' })}
-              />
-            </Form.Item>
+            <Col span={16}>
+              <Form.Item
+                label={formatMessage({ id: 'cases-new.form.responsible-users.label' })}
+                name="notified-users"
+              >
+                <Select
+                  mode="multiple"
+                  placeholder={formatMessage({
+                    id: 'cases-new.form.responsible-users.placeholder',
+                  })}
+                />
+              </Form.Item>
+            </Col>
           </Row>
           <Row>
-            <Button type="primary" onClick={onSubmit}>
-              <FormattedMessage id="cases-new.form.save.label" />
-            </Button>
+            <Col span={16}>
+              <Form.Item {...tailLayout}>
+                <Button type="primary" onClick={onSubmit}>
+                  <FormattedMessage id="cases-new.form.save.label" />
+                </Button>
+              </Form.Item>
+            </Col>
           </Row>
         </Card>
       </PageHeaderWrapper>
