5caf1dc50a1ec877ad9916dc7c393c943ff75901
kkm <kubasurdej@gmail.com>
2020-04-10 23:14:37 +0200

    Move plugin config to TypeScript

--- frontend-project/config/plugin.config.ts
+++ frontend-project/config/plugin.config.ts
@@ -1,28 +1,30 @@
 import path from 'path';
 
-function getModulePackageName(module) {
-  if (!module.context) return null;
-  const nodeModulesPath = path.join(__dirname, '../node_modules/');
+import * as IWebpackChainConfig from 'webpack-chain';
 
+function getModulePackageName(module: { context: string }) {
+  if (!module.context) return null;
+
+  const nodeModulesPath = path.join(__dirname, '../node_modules/');
   if (module.context.substring(0, nodeModulesPath.length) !== nodeModulesPath) {
     return null;
   }
 
   const moduleRelativePath = module.context.substring(nodeModulesPath.length);
   const [moduleDirName] = moduleRelativePath.split(path.sep);
-  let packageName = moduleDirName; // handle tree shaking
-
+  let packageName: string | null = moduleDirName;
+  // handle tree shaking
   if (packageName && packageName.match('^_')) {
     // eslint-disable-next-line prefer-destructuring
-    packageName = packageName.match(/^_(@?[^@]+)/)[1];
+    packageName = packageName.match(/^_(@?[^@]+)/)![1];
   }
-
   return packageName;
 }
 
-export const webpackPlugin = config => {
+const webpackPlugin = (config: IWebpackChainConfig) => {
   // optimize chunks
-  config.optimization // share the same chunks across different modules
+  config.optimization
+    // share the same chunks across different modules
     .runtimeChunk(false)
     .splitChunks({
       chunks: 'async',
@@ -31,35 +33,33 @@ export const webpackPlugin = config => {
       minSize: 0,
       cacheGroups: {
         vendors: {
-          test: module => {
+          test: (module: { context: string }) => {
             const packageName = getModulePackageName(module) || '';
-
             if (packageName) {
               return [
                 'bizcharts',
                 'gg-editor',
                 'g6',
                 '@antv',
+                'l7',
                 'gg-editor-core',
                 'bizcharts-plugin-slider',
               ].includes(packageName);
             }
-
             return false;
           },
-
-          name(module) {
+          name(module: { context: string }) {
             const packageName = getModulePackageName(module);
-
             if (packageName) {
               if (['bizcharts', '@antv_data-set'].indexOf(packageName) >= 0) {
                 return 'viz'; // visualization package
               }
             }
-
             return 'misc';
           },
         },
       },
     });
 };
+
+export default webpackPlugin;
