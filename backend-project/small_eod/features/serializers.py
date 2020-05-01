b4e3f2623b2d3558c9b5f88a98e4f42716f27645
Hubert <h.kaczmarek90@gmail.com>
2020-05-01 14:22:39 +0200

    Plural_form_features

--- backend-project/small_eod/features/serializers.py
+++ backend-project/small_eod/features/serializers.py
@@ -5,13 +5,13 @@ from rest_framework.serializers import ModelSerializer
 
 
 class FeatureOptionSerializer(ModelSerializer):
-    feature = serializers.PrimaryKeyRelatedField(
+    features = serializers.PrimaryKeyRelatedField(
         many=False, default=None, queryset=Feature.objects.all()
     )
 
     class Meta:
         model = FeatureOption
-        fields = ["id", "name", "feature"]
+        fields = ["id", "name", "features"]
 
 
 class NestedFeatureOptionSerializer(ModelSerializer):
@@ -29,7 +29,7 @@ class FeatureSerializer(UserLogModelSerializer):
 
     def create(self, validated_data):
         features_data = validated_data.pop("featureoptions")
-        feature = super().create(validated_data)
+        features = super().create(validated_data)
         for feature_data in features_data:
-            FeatureOption.objects.create(feature=feature, **feature_data)
-        return feature
+            FeatureOption.objects.create(features=features, **feature_data)
+        return features
