cc21c3a6e8ae6345265235126d30337e7aa435c4
mateuszmagulski <mateusz.magulski@gmail.com>
2020-05-04 20:44:47 +0200

    Add institution form

---
+++ frontend-project/src/pages/institutions/new/locales/en-US.js
@@ -0,0 +1,16 @@
+export default {
+  'institutions-new.form.name.label': 'Name',
+  'institutions-new.form.name.placeholder': "Institution's name",
+  'institutions-new.form.name.required-error': 'Name is required',
+  'institutions-new.form.save.label': 'Save',
+  'institutions-new.form.administrative-division.label': 'Administrative Division',
+  'institutions-new.form.administrative-division.placeholder': 'Choose administrative division',
+  'institutions-new.form.administrative-division.required-error': 'Administrative division is required',
+  'institutions-new.page-header-content': 'You can add new institution here.',
+  'institutions-new.form.address.label': 'Address',
+  'institutions-new.form.address.placeholder': 'Choose address',
+  'institutions-new.form.address.required-error': 'Address is required',
+  'institutions-new.form.external-identifier.label': 'External identifier',
+  'institutions-new.form.external-identifier.placeholder': 'Choose external identifier',
+  'institutions-new.form.external-identifier.required-error': 'External identifier is required',
+};
