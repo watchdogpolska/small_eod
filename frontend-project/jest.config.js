e8de165047c225e4e5f857f4c5bfbecb2f652f63
kkm <kubasurdej@gmail.com>
2020-05-13 01:39:49 +0200

    Modify jest config to transform files using ts-jest

--- frontend-project/jest.config.js
+++ frontend-project/jest.config.js
@@ -6,4 +6,8 @@ module.exports = {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
     localStorage: null,
   },
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testMatch: ['**/?(*.)+(spec|test).[t]s'],
 };
