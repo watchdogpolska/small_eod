3922a385ff3ad81d36a55c0bd4d42deb203ebe57
kkm <kubasurdej@gmail.com>
2020-05-13 01:53:13 +0200

    Delete utils tests not making sense because of type check

--- frontend-project/src/utils/utils.test.ts
+++ frontend-project/src/utils/utils.test.ts
@@ -1,16 +1,6 @@
 import { isUrl, getRouteAuthority } from './utils';
 
 describe('isUrl tests', () => {
-  it('should return false for invalid and corner case inputs', () => {
-    expect(isUrl([])).toBeFalsy();
-    expect(isUrl({})).toBeFalsy();
-    expect(isUrl(false)).toBeFalsy();
-    expect(isUrl(true)).toBeFalsy();
-    expect(isUrl(NaN)).toBeFalsy();
-    expect(isUrl(null)).toBeFalsy();
-    expect(isUrl(undefined)).toBeFalsy();
-    expect(isUrl('')).toBeFalsy();
-  });
   it('should return false for invalid URLs', () => {
     expect(isUrl('foo')).toBeFalsy();
     expect(isUrl('bar')).toBeFalsy();
