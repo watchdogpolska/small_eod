864828bb8267253c23a646faf5f6fd9d47154b03
kkm <kubasurdej@gmail.com>
2020-05-13 17:46:38 +0200

    Delete files related to e2e tests

--- frontend-project/jest.config.js
+++ frontend-project/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  testURL: 'http://localhost:8000',
-  preset: 'jest-puppeteer',
-  extraSetupFiles: ['./tests/setupTests.js'],
-  globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
-    localStorage: null,
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
