48c92719e2b3d4d1f7d396de2c46699722a1d1dd
Adam Dobrawy <ad-m@users.noreply.github.com>
2020-05-14 19:13:58 +0200

    Add simple support for OAuth (#300)
    * #104 Initial solution proposal
    
    * Lint fixes
    
    * Add missing migration for files app
    
    * Simplify code for files, use serializers
    
    * Remove unindented changes in Makefile
    
    * Small formatting issue in FileSerializer
    
    * Fix non-used import of 'redirect'
    
    * Fix lint issues
    
    * Fix startup Minio for check on GitHub Actions
    
    * Fixes for /letter/file/sign end-point
    
    * Update backend-project/config/urls.py
    
    Co-Authored-By: Adam Dobrawy <ad-m@users.noreply.github.com>
    
    * Lint fixes
    
    * FileViewSet inheritance changed to ModelViewSet.
    
    * Again lint fix.
    
    * Just a try to test files end-points
    
    * Add e2e tests for file upload & small tweaks
    
    * Fix formatting
    
    * Initial google auth integration proposal for #97
    
    * Documentation draft
    
    * Flow description
    
    * Links to external documentation added.
    
    * Simplify Google integration
    
    * backend: Fix authentication for collection
    
    * Fix formatting
    
    * backend: Add mocked tests for authententication
    
    * Fix formatting
    
    Co-authored-by: matiuszka <StqY2boeiZ>
    Co-authored-by: matiuszka <40184215+matiuszka@users.noreply.github.com>
    Co-authored-by: matiuszka <nowak.mateusz@hotmail.com>

--- backend-project/small_eod/users/views.py
+++ backend-project/small_eod/users/views.py
@@ -1,6 +1,17 @@
 from rest_framework import viewsets
-
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import UserSerializer, User
+from django.conf import settings
+from .serializers import (
+    RequestSerializer,
+    TokenResponseSerializer,
+    RefreshTokenRequestSerializer,
+)
+from rest_framework_simplejwt.tokens import RefreshToken
+from .providers import GoogleProvider
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,3 +21,74 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    provider = GoogleProvider(
+        client_id=settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY,
+        client_secret=settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET,
+        scopes=settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE,
+    )
+
+    def get_permissions(self):
+        if self.action in ["auth", "exchange", "refresh"]:
+            return [
+                AllowAny(),
+            ]
+        return super().get_permissions()
+
+    @swagger_auto_schema(
+        method="get",
+        operation_description="API endpoint to receive URI for OAuth authorization url",
+        responses={200: RequestSerializer()},
+        manual_parameters=[],
+        security=[],
+    )
+    @action(detail=False)
+    def auth(self, request):
+        authorization_url, state = self.provider.callback_url(request)
+        request.session["state"] = state
+        serializer = RequestSerializer({"url": authorization_url})
+        return Response(serializer.data)
+
+    @action(detail=False)
+    @swagger_auto_schema(
+        operation_description="API endpoint to exchange "
+        + "authorization code to access token",
+        responses={200: TokenResponseSerializer()},
+        manual_parameters=[],
+        security=[],
+    )
+    def exchange(self, request):
+        profile = self.provider.exchange(request)
+        user, _ = User.objects.get_or_create(
+            defaults={
+                "username": profile["email"],
+                "first_name": profile["given_name"],
+                "last_name": profile["family_name"],
+                "email": profile["email"],
+            },
+            email=profile["email"],
+        )
+        refresh = RefreshToken.for_user(user)
+        serializer = TokenResponseSerializer(
+            {"refresh_token": str(refresh), "access_token": str(refresh.access_token)}
+        )
+        return Response(serializer.data)
+
+    @action(detail=False, methods=["post"])
+    @swagger_auto_schema(
+        operation_description="API endpoint to exchange "
+        + "refresh token to fresh access token",
+        responses={200: TokenResponseSerializer()},
+        request_body=RefreshTokenRequestSerializer,
+        manual_parameters=[],
+        security=[],
+    )
+    def refresh(self, request):
+        serializer_input = RefreshTokenRequestSerializer(data=request.data)
+        serializer_input.is_valid(raise_exception=True)
+        refresh = RefreshToken(serializer_input.validated_data["refresh_token"])
+        refresh.set_jti()
+        refresh.set_exp()
+        serializer = TokenResponseSerializer(
+            {"refresh_token": str(refresh), "access_token": str(refresh.access_token)}
+        )
+        return Response(serializer.data)
