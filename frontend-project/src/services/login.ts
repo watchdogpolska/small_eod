8b9084265fc74a07086db65930e2b7ae7f391524
kkm <kubasurdej@gmail.com>
2020-05-21 18:49:13 +0200

    Move login service to typescript

--- frontend-project/src/services/login.ts
+++ frontend-project/src/services/login.ts
@@ -1,11 +1,12 @@
 import request from '@/utils/request';
 
-export async function fakeAccountLogin(params) {
+export const fakeAccountLogin = async (params: unknown) => {
   return request('/api/login/account', {
     method: 'POST',
     data: params,
   });
-}
-export async function getFakeCaptcha(mobile) {
+};
+
+export const getFakeCaptcha = async (mobile: string) => {
   return request(`/api/login/captcha?mobile=${mobile}`);
-}
+};
