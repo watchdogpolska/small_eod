9719af658d968fa03c67878f15934d2d636d376c
kkm <kubasurdej@gmail.com>
2020-05-21 19:45:14 +0200

    Move login service to ts, delete captcha function

--- frontend-project/src/services/login.ts
+++ frontend-project/src/services/login.ts
@@ -1,12 +1,21 @@
 import request from '@/utils/request';
 
-export const fakeAccountLogin = async (params: unknown) => {
+export interface LoginParams {
+  userName: string;
+  password: string;
+  mobile: string;
+  captcha: string;
+}
+
+export interface FakeAccountLoginResponse {
+  status: 'ok' | 'error';
+  type: string;
+  currentAuthority: string;
+}
+
+export const fakeAccountLogin = async (params: LoginParams): Promise<FakeAccountLoginResponse> => {
   return request('/api/login/account', {
     method: 'POST',
     data: params,
   });
-};
-
-export const getFakeCaptcha = async (mobile: string) => {
-  return request(`/api/login/captcha?mobile=${mobile}`);
 };
