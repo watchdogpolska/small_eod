6c902a7a7f0efc7e26cac446c9c8083b674456f8
kkm <kubasurdej@gmail.com>
2020-05-29 15:46:34 +0200

    Delete register and register-result pages

--- frontend-project/config/config.ts
+++ frontend-project/config/config.ts
@@ -88,18 +88,6 @@ export default {
               component: './user/login',
             },
             {
-              name: 'register-result',
-              icon: 'smile',
-              path: '/user/register-result',
-              component: './user/register-result',
-            },
-            {
-              name: 'register',
-              icon: 'smile',
-              path: '/user/register',
-              component: './user/register',
-            },
-            {
               component: '404',
             },
           ],
