368d10e1f807d3ddef2d05a08d06956c757ca4e8
kkm <kubasurdej@gmail.com>
2020-05-29 16:55:56 +0200

    Migrate LoginContext, LoginIntem and Login/index to tsx

--- frontend-project/src/pages/user/login/components/Login/LoginItem.tsx
+++ frontend-project/src/pages/user/login/components/Login/LoginItem.tsx
@@ -1,33 +1,66 @@
 import { Button, Col, Input, Row, Form, message } from 'antd';
 import React, { useState, useCallback, useEffect } from 'react';
+
 import omit from 'omit.js';
+import { FormItemProps } from 'antd/es/form/FormItem';
 import { getFakeCaptcha } from '@/services/login';
 import ItemMap from './map';
-import LoginContext from './LoginContext';
+import LoginContext, { LoginContextProps } from './LoginContext';
 import styles from './index.less';
+
+export type WrappedLoginItemProps = LoginItemProps;
+export type LoginItemKeyType = keyof typeof ItemMap;
+export interface LoginItemType {
+  UserName: React.FC<WrappedLoginItemProps>;
+  Password: React.FC<WrappedLoginItemProps>;
+  Mobile: React.FC<WrappedLoginItemProps>;
+  Captcha: React.FC<WrappedLoginItemProps>;
+}
+
+export interface LoginItemProps extends Partial<FormItemProps> {
+  name?: string;
+  style?: React.CSSProperties;
+  placeholder?: string;
+  buttonText?: React.ReactNode;
+  countDown?: number;
+  getCaptchaButtonText?: string;
+  getCaptchaSecondText?: string;
+  updateActive?: LoginContextProps['updateActive'];
+  type?: string;
+  defaultValue?: string;
+  customProps?: { [key: string]: unknown };
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  tabUtil?: LoginContextProps['tabUtil'];
+}
 
 const FormItem = Form.Item;
 
-const getFormItemOptions = ({ onChange, defaultValue, customProps = {}, rules }) => {
-  const options = {
-    rules: rules || customProps.rules,
+const getFormItemOptions = ({
+  onChange,
+  defaultValue,
+  customProps = {},
+  rules,
+}: LoginItemProps) => {
+  const options: {
+    rules?: LoginItemProps['rules'];
+    onChange?: LoginItemProps['onChange'];
+    initialValue?: LoginItemProps['defaultValue'];
+  } = {
+    rules: rules || (customProps.rules as LoginItemProps['rules']),
   };
-
   if (onChange) {
     options.onChange = onChange;
   }
-
   if (defaultValue) {
     options.initialValue = defaultValue;
   }
-
   return options;
 };
 
-const LoginItem = props => {
-  const [count, setCount] = useState(props.countDown || 0);
+const LoginItem: React.FC<LoginItemProps> = props => {
+  const [count, setCount] = useState<number>(props.countDown || 0);
   const [timing, setTiming] = useState(false);
-
+  // 这么写是为了防止restProps中 带入 onChange, defaultValue, rules props tabUtil
   const {
     onChange,
     customProps,
@@ -41,46 +74,44 @@ const LoginItem = props => {
     tabUtil,
     ...restProps
   } = props;
-  const onGetCaptcha = useCallback(async mobile => {
-    const result = await getFakeCaptcha(mobile);
 
+  const onGetCaptcha = useCallback(async (mobile: string) => {
+    const result = await getFakeCaptcha(mobile);
     if (result === false) {
       return;
     }
-
-    message.success('Successfully acquired verification code：1234');
+    message.success('获取验证码成功！验证码为：1234');
     setTiming(true);
   }, []);
-  useEffect(() => {
-    let interval = 0;
-    const { countDown } = props;
 
+  useEffect(() => {
+    let interval: number = 0;
+    const { countDown } = props;
     if (timing) {
       interval = window.setInterval(() => {
         setCount(preSecond => {
           if (preSecond <= 1) {
             setTiming(false);
             clearInterval(interval);
+            // 重置秒数
             return countDown || 60;
           }
-
           return preSecond - 1;
         });
       }, 1000);
     }
-
     return () => clearInterval(interval);
   }, [timing]);
-
   if (!name) {
     return null;
-  } // get getFieldDecorator props
-
+  }
+  // get getFieldDecorator props
   const options = getFormItemOptions(props);
   const otherProps = restProps || {};
 
   if (type === 'Captcha') {
     const inputProps = omit(otherProps, ['onGetCaptcha', 'countDown']);
+
     return (
       <FormItem shouldUpdate>
         {({ getFieldValue }) => (
@@ -100,7 +131,7 @@ const LoginItem = props => {
                   onGetCaptcha(value);
                 }}
               >
-                {timing ? `${count} seconds` : 'Get verification code'}
+                {timing ? `${count} 秒` : '获取验证码'}
               </Button>
             </Col>
           </Row>
@@ -108,7 +139,6 @@ const LoginItem = props => {
       </FormItem>
     );
   }
-
   return (
     <FormItem name={name} {...options}>
       <Input {...customProps} {...otherProps} />
@@ -116,11 +146,11 @@ const LoginItem = props => {
   );
 };
 
-const LoginItems = {};
+const LoginItems: Partial<LoginItemType> = {};
+
 Object.keys(ItemMap).forEach(key => {
   const item = ItemMap[key];
-
-  LoginItems[key] = props => (
+  LoginItems[key] = (props: LoginItemProps) => (
     <LoginContext.Consumer>
       {context => (
         <LoginItem
@@ -135,4 +165,5 @@ Object.keys(ItemMap).forEach(key => {
     </LoginContext.Consumer>
   );
 });
-export default LoginItems;
+
+export default LoginItems as LoginItemType;
