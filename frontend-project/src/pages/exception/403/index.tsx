433fa47ae407bf6d518b6c5b6ae6719560fed4e4
kuskoman <kubasurdej@gmail.com>
2020-05-29 10:16:25 +0200

    Zmiana rozszerzenia stron wyjątków na tsx + dodanie wersji polskiej (#376)

--- frontend-project/src/pages/exception/403/index.tsx
+++ frontend-project/src/pages/exception/403/index.tsx
@@ -1,24 +1,24 @@
 import Link from 'umi/link';
 import { Result, Button } from 'antd';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { formatMessage } from 'umi-plugin-react/locale';
 
-export default () => (
+export default (): ReactNode => (
   <Result
-    status="500"
-    title="500"
+    status="403"
+    title="403"
     style={{
       background: 'none',
     }}
     subTitle={formatMessage({
-      id: 'exceptionand500.description.500',
-      defaultMessage: 'Sorry, the server is reporting an error.',
+      id: 'exceptionand403.description.403',
+      defaultMessage: "Sorry, you don't have access to this page.",
     })}
     extra={
       <Link to="/">
         <Button type="primary">
           {formatMessage({
-            id: 'exceptionand500.exception.back',
+            id: 'exceptionand403.exception.back',
             defaultMessage: 'Back Home',
           })}
         </Button>
---
+++ frontend-project/src/pages/exception/500/locales/pl-PL.ts
@@ -0,0 +1,4 @@
+export default {
+  'exceptionand500.exception.back': 'Wróć do strony startowej',
+  'exceptionand500.description.500': 'Nastąpił nieoczekiwany błąd serwera.',
+};
