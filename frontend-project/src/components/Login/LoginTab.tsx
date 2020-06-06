98b5da5f51ca7e6407f9f56b067b4519e3fa57e6
kuskoman <kubasurdej@gmail.com>
2020-06-06 20:26:24 +0200

    Migracja pages do tsx i przeniesienie części zawartości do @/components (#381)

--- frontend-project/src/components/Login/LoginTab.tsx
+++ frontend-project/src/components/Login/LoginTab.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
+import { TabPaneProps } from 'antd/es/tabs';
 import { Tabs } from 'antd';
-import LoginContext from './LoginContext';
+import LoginContext, { LoginContextProps } from './LoginContext';
 
 const { TabPane } = Tabs;
 
@@ -12,24 +13,32 @@ const generateId = (() => {
   };
 })();
 
-const LoginTab = props => {
+interface LoginTabProps extends TabPaneProps {
+  tabUtil: LoginContextProps['tabUtil'];
+  active?: boolean;
+}
+
+const LoginTab: React.FC<LoginTabProps> = props => {
   useEffect(() => {
     const uniqueId = generateId('login-tab-');
     const { tabUtil } = props;
-
     if (tabUtil) {
       tabUtil.addTab(uniqueId);
     }
   }, []);
+
   const { children } = props;
-  return <TabPane {...props}>{children}</TabPane>;
+  return <TabPane {...props}>{props.active && children}</TabPane>;
 };
 
-const WrapContext = props => (
+const WrapContext: React.FC<TabPaneProps> & {
+  typeName: string;
+} = props => (
   <LoginContext.Consumer>
     {value => <LoginTab tabUtil={value.tabUtil} {...props} />}
   </LoginContext.Consumer>
-); // 标志位 用来判断是不是自定义组件
+);
 
 WrapContext.typeName = 'LoginTab';
+
 export default WrapContext;
