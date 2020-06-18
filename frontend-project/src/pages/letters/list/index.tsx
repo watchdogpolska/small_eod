6bdc7e069d10c820377626049edb502aa8453458
mateuszmagulski <mateusz.magulski@gmail.com>
2020-06-18 16:30:19 +0200

    channel

--- frontend-project/src/pages/letters/list/index.tsx
+++ frontend-project/src/pages/letters/list/index.tsx
@@ -3,6 +3,7 @@ import ProTable, { ProColumns, ActionType } from '@ant-design/pro-table';
 import React, { FC, useRef } from 'react';
 import { formatMessage } from 'umi-plugin-react/locale';
 
+import { ChannelName } from '@/components/Table/ChannelName';
 import { Letter, fetchLettersPage } from '@/services/letters';
 
 const TableList: FC<{}> = () => {
@@ -23,6 +24,12 @@ const TableList: FC<{}> = () => {
     {
       title: formatMessage({ id: 'letters-list.table.columns.direction.title' }),
       dataIndex: 'direction',
+    },
+    {
+      title: formatMessage({ id: 'letters-list.table.columns.channel.title' }),
+      dataIndex: 'channel',
+      render: (channel: number) =>
+        typeof channel === 'number' ? <ChannelName id={channel} /> : channel,
     },
   ];
 
--- frontend-project/src/pages/letters/list/locales/en-US.ts
+++ frontend-project/src/pages/letters/list/locales/en-US.ts
@@ -4,6 +4,7 @@ export default {
   'letters-list.table.columns.comment.title': 'Comment',
   'letters-list.table.columns.direction.title': 'Direction',
   'letters-list.table.columns.identifier.title': 'Identifier',
+  'letters-list.table.columns.channel.title': 'Channel',
   'letters-list.table.columns.name.title': 'Name',
   'letters-list.table.total': 'Total',
 };
