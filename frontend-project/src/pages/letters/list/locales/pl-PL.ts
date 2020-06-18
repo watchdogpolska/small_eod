41eadb4ce40a951ef9a57a6d6d85a1570d5121ea
mateuszmagulski <mateusz.magulski@gmail.com>
2020-06-18 13:40:24 +0200

    usunięcie kolumny name

--- frontend-project/src/pages/letters/list/locales/pl-PL.ts
+++ frontend-project/src/pages/letters/list/locales/pl-PL.ts
@@ -4,6 +4,5 @@ export default {
   'letters-list.table.columns.comment.title': 'Komentarz',
   'letters-list.table.columns.direction.title': 'Kierunek',
   'letters-list.table.columns.identifier.title': 'Identyfikator',
-  'letters-list.table.columns.name.title': 'Nazwa',
   'letters-list.table.total': 'Łącznie',
 };
