bd6e219f7f92d0d77f56f60a45a349f79f5421bb
mateuszmagulski <mateusz.magulski@gmail.com>
2020-06-18 14:52:27 +0200

    poprawienie nazw pl/en identyfikatora

--- frontend-project/src/pages/letters/list/locales/en-US.ts
+++ frontend-project/src/pages/letters/list/locales/en-US.ts
@@ -3,7 +3,7 @@ export default {
   'letters-list.table-header-title': 'Letters list',
   'letters-list.table.columns.comment.title': 'Comment',
   'letters-list.table.columns.direction.title': 'Direction',
-  'letters-list.table.columns.identifier.title': 'Identifier',
+  'letters-list.table.columns.identifier.title': 'Reference number',
   'letters-list.table.columns.name.title': 'Name',
   'letters-list.table.total': 'Total',
 };
