a05bfb2b2f3ee60f817e8efd4d13277b176de171
mateuszmagulski <52409408+mateuszmagulski@users.noreply.github.com>
2020-06-24 14:22:06 +0200

    migrate to Table component (#432)
    Co-authored-by: Adam Dobrawy <ad-m@users.noreply.github.com>

--- frontend-project/src/pages/letters/list/index.tsx
+++ frontend-project/src/pages/letters/list/index.tsx
@@ -1,15 +1,15 @@
-import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import ProTable, { ProColumns, ActionType } from '@ant-design/pro-table';
-import React, { FC, useRef } from 'react';
+import { ProColumns } from '@ant-design/pro-table';
+import React, { FC } from 'react';
 import { formatMessage } from 'umi-plugin-react/locale';
 
 import { ChannelName } from '@/components/Table/ChannelName';
 import { Letter, fetchLettersPage } from '@/services/letters';
+import Table from '@/components/Table';
+
 import { CaseName } from '@/components/Table/CaseName';
 import { InstitutionName } from '@/components/Table/InstitutionName';
 
 const TableList: FC<{}> = () => {
-  const actionRef = useRef<ActionType>();
   const columns: ProColumns<Letter>[] = [
     {
       title: formatMessage({ id: 'letters-list.table.columns.identifier.title' }),
@@ -64,29 +64,7 @@ const TableList: FC<{}> = () => {
     },
   ];
 
-  const showTotal = (total, range) =>
-    `${range[0]}-${range[1]} / ${formatMessage({ id: 'letters-list.table.total' })} ${total}`;
-
-  return (
-    <PageHeaderWrapper content={formatMessage({ id: 'letters-list.page-header-content' })}>
-      <ProTable<Letter>
-        headerTitle={formatMessage({ id: 'letters-list.table-header-title' })}
-        actionRef={actionRef}
-        rowKey="id"
-        tableAlertRender={false}
-        request={fetchLettersPage}
-        columns={columns}
-        rowSelection={false}
-        search={false}
-        options={false}
-        pagination={{
-          pageSize: 20,
-          showSizeChanger: false,
-          showTotal,
-        }}
-      />
-    </PageHeaderWrapper>
-  );
+  return <Table type="letters" columns={columns} fetchData={fetchLettersPage} />;
 };
 
 export default TableList;
