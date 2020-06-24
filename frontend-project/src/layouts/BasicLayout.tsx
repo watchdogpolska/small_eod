e492ab2789130fc879efdcae15ba89bfaacb8d01
Adam Dobrawy <ad-m@users.noreply.github.com>
2020-06-24 18:08:27 +0200

    Update BasicLayout.tsx

--- frontend-project/src/layouts/BasicLayout.tsx
+++ frontend-project/src/layouts/BasicLayout.tsx
@@ -7,7 +7,7 @@ import ProLayout, {
 } from '@ant-design/pro-layout';
 import React, { useEffect } from 'react';
 import { Link } from 'umi';
-// import { GithubOutlined } from '@ant-design/icons';
+import { GithubOutlined } from '@ant-design/icons';
 import { Result, Button } from 'antd';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
