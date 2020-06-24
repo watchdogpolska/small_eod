5b28d0eeaa8c3c6ad83cbdbae95b7fb35aeed60d
Adam Dobrawy <ad-m@users.noreply.github.com>
2020-06-24 18:08:02 +0200

    Update frontend-project/src/layouts/BasicLayout.tsx

--- frontend-project/src/layouts/BasicLayout.tsx
+++ frontend-project/src/layouts/BasicLayout.tsx
@@ -68,7 +68,7 @@ const defaultFooterDom = (
       },
       {
         key: 'github',
-        title: 'small-eod – GitHub', // <GithubOutlined />,
+        title: ( <> small-eod – GitHub <GithubOutlined /> </> ),
         href: 'https://github.com/watchdogpolska/small_eod/',
         blankTarget: true,
       },
