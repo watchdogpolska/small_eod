b62e71c53207fa9e13ecff2f7f9ea568207351ce
Kamil Breguła <kamil.bregula@polidea.com>
2020-06-26 11:31:36 +0200

    fixup! Add CaseAdmin

--- backend-project/small_eod/cases/tests/test_admin.py
+++ backend-project/small_eod/cases/tests/test_admin.py
@@ -1,24 +1,22 @@
 from django.test import TestCase
 from django.urls import reverse
 from ...users.mixins import AuthenticatedMixin
+from ..models import Case
 
 
 class TestAddAdminMixin:
     @property
-    def app_label(self):
-        raise NotImplementedError()
-
-    @property
-    def model_name(self):
+    def model(self):
         raise NotImplementedError()
 
     def get_url(self):
-        return reverse(f"admin:{self.app_label}_{self.model_name}_add")
+        return reverse(
+            f"admin:{self.model._meta.app_label}_{self.model._meta.model_name}_add"
+        )
 
 
 class TestCaseAddAdmin(TestAddAdminMixin, AuthenticatedMixin, TestCase):
-    app_label = "cases"
-    model_name = "case"
+    model = Case
 
     def test_should_display_form(self):
         self.login_required(is_staff=True, is_superuser=True)
