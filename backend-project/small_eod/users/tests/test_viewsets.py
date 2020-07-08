73e6f4bee4c1854a9797718192fd886fa5374a25
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-07-08 21:48:54 +0200

    Validate initial user count

--- backend-project/small_eod/users/tests/test_viewsets.py
+++ backend-project/small_eod/users/tests/test_viewsets.py
@@ -9,6 +9,7 @@ class UserViewSetTestCase(ReadOnlyViewSetMixin, TestCase):
     basename = "user"
     serializer_class = UserSerializer
     factory_class = UserFactory
+    initial_count = 1
 
     def validate_item(self, item):
         self.assertEqual(item["username"], self.obj.username)
