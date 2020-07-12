8b91f578b662ccf15419567d2e68ad4ba6445fae
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-07-12 04:32:02 +0200

    Fix formatting

--- backend-project/small_eod/tags/tests/test_serializers.py
+++ backend-project/small_eod/tags/tests/test_serializers.py
@@ -4,6 +4,7 @@ from ...generic.tests.test_serializers import ResourceSerializerMixin
 from ..factories import TagFactory
 from ..serializers import TagSerializer
 
+
 class TagSerializerTestCase(ResourceSerializerMixin, TestCase):
     serializer_class = TagSerializer
     factory_class = TagFactory
