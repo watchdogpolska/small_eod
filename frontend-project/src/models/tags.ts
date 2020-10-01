184127ca40ab11217d4f3e55d25e502952a2456f
kkm <kubasurdej@gmail.com>
2020-10-01 13:43:45 +0200

    Refactor tags model

--- frontend-project/src/models/tags.ts
+++ frontend-project/src/models/tags.ts
@@ -1,6 +1,8 @@
 import { fetchAll, fetchPage, Tag } from '@/services/tags';
-import { Effect, EffectsCommandMap } from 'dva';
-import { AnyAction, Reducer } from 'redux';
+import { Effect } from 'dva';
+import { Reducer } from 'redux';
+
+export type TagDefaultState = Tag[];
 
 export interface TagModelType {
   namespace: string;
@@ -10,24 +12,24 @@ export interface TagModelType {
     fetchPage: Effect;
   };
   reducers: {
-    saveAll: Reducer<Tag[], AnyAction>;
-    savePage: Reducer<Tag[], AnyAction>;
+    saveAll: Reducer<TagDefaultState>;
+    savePage: Reducer<TagDefaultState>;
   };
 }
-const defaultTagsState: Tag[] = [];
+const defaultTagsState: TagDefaultState = [];
 
 const TagsModel: TagModelType = {
   namespace: 'tags',
   state: defaultTagsState,
   effects: {
-    *fetchAll(_: AnyAction, { call, put }: EffectsCommandMap) {
+    *fetchAll(_, { call, put }) {
       const response = yield call(fetchAll);
       yield put({
         type: 'saveAll',
         payload: response,
       });
     },
-    *fetchPage({ payload }: AnyAction, { call, put }: EffectsCommandMap) {
+    *fetchPage({ payload }, { call, put }) {
       const response = yield call(fetchPage, payload);
       yield put({
         type: 'savePage',
