466a70c73faae45d5147bbc047b88523a8cfdd82
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-10-06 01:35:57 +0200

    Add interface generator scripts

---
+++ frontend-project/scripts/generate_interface.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const main = async () => {
+    if (process.argv.length < 3) {
+        console.error(`${process.argv.slice(0, 2).join("")} [specification-file]`);
+    }
+    const spec = JSON.parse(await fs.promises.readFile(process.argv[2]));
+    console.log('// This file is generated, do not manually edit this file');
+    console.log('// Update and use \'@/scripts/generate_interface.js\' instead');
+    console.log('');
+    for (const [name, schema] of Object.entries(spec.definitions)) {
+        console.log(`export interface ${name} {`);
+        for (const [pname, pvalue] of Object.entries(schema.properties)) {
+            if (pvalue.type == 'string') {
+                console.log(`  ${pname}: string;`);
+            } else if (pvalue.type == 'integer') {
+                console.log(`  ${pname}: number;`);
+            } else if (pvalue.type == 'boolean') {
+                console.log(`  ${pname}: boolean;`);
+            } else if (pvalue.type == 'array' && pvalue.items.type == 'integer') {
+                console.log(`  ${pname}: number[];`);
+            } else if (pvalue.type == 'array' && pvalue.items.type == 'string') {
+                console.log(`  ${pname}: string[];`);
+            } else if (pvalue.type == 'array' && pvalue.items['$ref']) {
+                const name = pvalue.items['$ref'].replace('#/definitions/', '');
+                console.log(`  ${pname}: ${name}[];`);
+            } else {
+                console.log(`  // Unsupported type '${pvalue.type}' in ${name}/${pname}`);
+                console.log(`  ${pname}: any;`);
+            }
+        }
+        console.log(`}`);
+        console.log(``);
+    }
+};
+
+main().catch(err => {
+    console.log(err);
+    process.exit(-1);
+})
