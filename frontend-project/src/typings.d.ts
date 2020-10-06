2685cfd793e2d6b7e45f0e3182cc4d385bc03184
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-10-06 02:02:06 +0200

    Futher remove ga

--- frontend-project/src/typings.d.ts
+++ frontend-project/src/typings.d.ts
@@ -13,19 +13,7 @@ declare module '*.tiff';
 declare module 'omit.js';
 
 // google analytics interface
-interface GAFieldsObject {
-  eventCategory: string;
-  eventAction: string;
-  eventLabel?: string;
-  eventValue?: number;
-  nonInteraction?: boolean;
-}
 interface Window {
-  ga: (
-    command: 'send',
-    hitType: 'event' | 'pageview',
-    fieldsObject: GAFieldsObject | string,
-  ) => void;
   reloadAuthorized: () => void;
 }
 
