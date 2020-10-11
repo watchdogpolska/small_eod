69671d51d72527d58c7e3b383ee4449015d564d6
Daxter44 <marcinek44@windowslive.com>
2020-10-11 22:44:06 +0200

    Add features list page

---
+++ frontend-project/src/services/features.ts
@@ -0,0 +1,21 @@
+import { PaginationParams, PaginationResponse } from '@/services/common.d';
+import smallEodSDK from '@/utils/sdk';
+import { Feature } from './definitions';
+
+export async function fetchFeaturesPage({ current, pageSize }: PaginationParams): Promise<PaginationResponse<Feature>> {
+    smallEodSDK.FeaturesApi();
+
+    const sdkResponse = await smallEodSDK.featuresList({
+        limit: pageSize,
+        offset: pageSize * (current - 1),
+    });
+
+    return {
+        data: sdkResponse.results,
+        total: sdkResponse.count,
+    };
+}
+
+export const fetchOne = async (id: number): Promise<Feature> => {
+    return new smallEodSDK.FeaturesApi().featuresRead(id);
+};
