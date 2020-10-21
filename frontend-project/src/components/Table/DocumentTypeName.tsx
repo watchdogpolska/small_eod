1a4cf59e9fa52e3a3b18b9fd53a0faf7741550d1
mateuszmagulski <52409408+mateuszmagulski@users.noreply.github.com>
2020-10-21 19:57:29 +0200

    Przeniesienie typu dokumentu do reduxa w komponencie DocumentTypeName (#476)
    * documentType in redux
    
    * lint
    
    * add type to documentTypes
    
    * prettier
    
    * fix import
    
    Co-authored-by: Mateusz Magulski <mateusz.magulski@stxnext.pl>

--- frontend-project/src/components/Table/DocumentTypeName.tsx
+++ frontend-project/src/components/Table/DocumentTypeName.tsx
@@ -1,16 +1,23 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Spin } from 'antd';
+import { connect } from 'dva';
 
-import { fetchDocumentType } from '@/services/documentType';
+import { DocumentType } from '@/models/documentTypes';
 
-export const DocumentTypeName: FC<{ id: number }> = props => {
-  const [name, setName] = useState('');
+export interface DocumentTypeNameProps {
+  id: number;
+  documentTypes: DocumentType[];
+  dispatch: Function;
+}
+
+const DocumentTypeName: FC<DocumentTypeNameProps> = ({ id, documentTypes, dispatch }) => {
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetchDocumentType(props.id);
-      setName(result.name);
-    };
-    fetchData();
+    dispatch({ type: 'documentTypes/fetchOne', payload: id });
   }, []);
-  return name ? <div>{name}</div> : <Spin />;
+  const documentType = documentTypes.find(value => value.id === id);
+
+  return <div>{documentType ? documentType.name : <Spin />}</div>;
 };
+export default connect(({ documentTypes }: DocumentTypeNameProps) => ({ documentTypes }))(
+  DocumentTypeName,
+);
