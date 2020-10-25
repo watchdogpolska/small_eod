f3ec0bf95f7f5ff4a280432417ec2b4fb6d084b3
Adam Dobrawy <ad-m@users.noreply.github.com>
2020-10-25 16:23:52 +0100

    Add iCalendar export (#686)
    * Add iCalendar export
    
    * Fix formatting

--- backend-project/small_eod/events/tests/test_views.py
+++ backend-project/small_eod/events/tests/test_views.py
@@ -17,3 +17,16 @@ class EventViewSetTestCase(GenericViewSetMixin, OrderingViewSetMixin, TestCase):
 
     def validate_item(self, item):
         self.assertEqual(item["name"], self.obj.name)
+
+    def test_render_as_ical(self):
+        self.login_required()
+
+        response = self.client.get(
+            self.get_url(name="ical", **self.get_extra_kwargs()),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get("Content-Type"), "text/calendar")
+        body = response.content.decode("utf-8")
+        self.assertIn(self.obj.name, body)
+        self.assertIn(self.obj.comment, body)
+        self.assertIn(self.obj.case.name, body)
