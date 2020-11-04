f91aad78b68a954f33bc39acaef33bda28b0307e
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-11-04 05:54:58 +0100

    Fix formatting

--- small_eod/events/models.py
+++ small_eod/events/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from ..cases.models import Case
 from ..authkey.models import TimestampUserLogModel
-from django.conf import settings
 
 
 class Event(TimestampUserLogModel):
