c269440774d188a3be41ffbeb246b3e18a55c518
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-11-14 05:58:31 +0100

    Fix standard compilance for calendar

--- small_eod/events/views.py
+++ small_eod/events/views.py
@@ -2,7 +2,6 @@ from icalendar import Calendar, Event as IEvent
 from django.urls import reverse
 from django.template import loader
 from django.http import HttpResponse
-from django.utils.translation import ugettext as _
 from .models import Event
 from django.http import HttpResponseForbidden
 from ..authkey.parser import get_token
@@ -12,9 +11,8 @@ calendar_description_template = "events/calendar_description.txt"
 
 
 def ical(request):
-    cal = Calendar()
-    cal["summary"] = _("small-eod - events")
     t = loader.get_template(calendar_description_template)
+
     token = get_token(request)
     if not token:
         return HttpResponseForbidden("Missing authentication token")
@@ -25,6 +23,10 @@ def ical(request):
     if not key.has_scopes(["export_ical"]):
         return HttpResponseForbidden("Unauthorized operation for the token")
     key.update_used_on()
+
+    cal = Calendar()
+    cal.add("prodid", f"-//small-eod//events//{key.user}//")
+    cal.add("version", "2.0")
     for obj in Event.objects.select_related("case", "case__audited_institution"):
         event = IEvent()
         url = request.build_absolute_uri(
@@ -34,11 +36,12 @@ def ical(request):
         categories = [obj.case]
         if obj.case.audited_institution:
             categories.append(obj.case.audited_institution)
-        event.add("uid", obj.pk)
+        event.add("uid", url)
         event.add("dtstart", obj.date)
+        event.add("dtstamp", obj.date)
         event.add("summary", obj.name)
         event.add("created", obj.created_on)
-        event.add("last-mod", obj.modified_on)
+        event.add("last-modified", obj.modified_on)
         event.add("url", url)
         event.add("description", description)
         event.add("categories", categories)
