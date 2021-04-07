4297a5881c8c3f650a4f045835d18a41a1c806b5
Michal Karol <michal.p.karol@gmail.com>
2021-04-07 14:36:38 +0200

    Fixing linting

--- backend-project/small_eod/features/tests/test_serializers.py
+++ backend-project/small_eod/features/tests/test_serializers.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from ...generic.mixins import AuthRequiredMixin
 from ...generic.tests.test_serializers import ResourceSerializerMixin
 from ..factories import FeatureFactory
-from ..models import FeatureOption
 from ..serializers import FeatureSerializer
 
 
