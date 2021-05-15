61a2509dc38a08e5237af11f06d758a14ad33464
Adishone <adishoneee@gmail.com>
2021-05-15 23:43:56 +0200

    614-AddFaviconsAndLogoFile

--- frontend-project/src/layouts/LoginLayout.tsx
+++ frontend-project/src/layouts/LoginLayout.tsx
@@ -43,6 +43,13 @@ const LoginLayout: FC<LoginLayoutProps> = props => {
       <Helmet>
         <title>{title}</title>
         <meta name="description" content={title} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Helmet>
 
       <div className={styles.container}>
